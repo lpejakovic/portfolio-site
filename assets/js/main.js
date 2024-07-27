368062e1902941f7164b41604fcf2d2925bd2e67
lpejakovic <luka.pejakovic@gmail.com>
2024-07-27 22:46:09 +0200

    Added Home section and preperation for About section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
-/**
-** Scroll top button
-**/
-
+/** Scroll top button */
 let scrollTop = document.querySelector('.scroll-top');
 function toggleScrollTop() {
   if (scrollTop) {
@@ -17,3 +14,28 @@ scrollTop.addEventListener('click', (e) => {
 });
 window.addEventListener('load', toggleScrollTop);
 document.addEventListener('scroll', toggleScrollTop);
+
+/** Init AOS.js */
+function aosInit() {
+  AOS.init({
+    duration: 600,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
+}
+window.addEventListener('load', aosInit);
+
+/** Init Typed.js */
+const selectTyped = document.querySelector('.typed');
+if (selectTyped) {
+  let typed_strings = selectTyped.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
