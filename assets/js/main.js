07e8dd2045e57478e8eb1b1938a05bd05fd83bc0
lpejakovic <luka.pejakovic@gmail.com>
2024-07-28 00:10:43 +0200

    Added sklill and resume section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,101 @@
-/** Scroll top button */
-let scrollTop = document.querySelector('.scroll-top');
-function toggleScrollTop() {
-  if (scrollTop) {
-    window.scrollY > 0 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+(function () {
+  "use strict";
+
+   /** Header toggle */
+   const headerToggleBtn = document.querySelector('.header-toggle');
+
+   function headerToggle() {
+     document.querySelector('#header').classList.toggle('header-show');
+     headerToggleBtn.classList.toggle('fa-bars');
+     headerToggleBtn.classList.toggle('fa-times');
+   }
+   headerToggleBtn.addEventListener('click', headerToggle);
+ 
+   /** Hide mobile nav on same-page/hash links */
+   document.querySelectorAll('#navmenu a').forEach(navmenu => {
+     navmenu.addEventListener('click', () => {
+       if (document.querySelector('.header-show')) {
+         headerToggle();
+       }
+     });
+ 
+   });
+
+  /** Scroll top button */
+  let scrollTop = document.querySelector('.scroll-top');
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 0 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
   }
-}
-scrollTop.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   });
-});
-window.addEventListener('load', toggleScrollTop);
-document.addEventListener('scroll', toggleScrollTop);
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
 
-/** Init AOS.js */
-function aosInit() {
-  AOS.init({
-    duration: 600,
-    easing: 'ease-in-out',
-    once: true,
-    mirror: false
-  });
-}
-window.addEventListener('load', aosInit);
+  /** Init AOS.js */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
 
-/** Init Typed.js */
-const selectTyped = document.querySelector('.typed');
-if (selectTyped) {
-  let typed_strings = selectTyped.getAttribute('data-typed-items');
-  typed_strings = typed_strings.split(',');
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000
+  /** Init Typed.js */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /** Correct scrolling position upon page load for URLs containing hash links. */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
   });
-}
+
+  /**  Navmenu Scrollspy */
+  let navmenulinks = document.querySelectorAll('.navmenu a');
+
+  function navmenuScrollspy() {
+    navmenulinks.forEach(navmenulink => {
+      if (!navmenulink.hash) return;
+      let section = document.querySelector(navmenulink.hash);
+      if (!section) return;
+      let position = window.scrollY + 200;
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+        navmenulink.classList.add('active');
+      } else {
+        navmenulink.classList.remove('active');
+      }
+    })
+  }
+  window.addEventListener('load', navmenuScrollspy);
+  document.addEventListener('scroll', navmenuScrollspy);
+})();
