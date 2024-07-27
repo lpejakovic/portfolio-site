aac69fa56bea532e89fa2e8574c5be67f52b6524
lpejakovic <luka.pejakovic@gmail.com>
2024-07-27 20:22:58 +0200

    Added Animate on scroll css and js (AOS) dependency

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
- /**
- ** Scroll top button
- **/
- let scrollTop = document.querySelector('.scroll-top');
+/**
+** Scroll top button
+**/
 
- function toggleScrollTop() {
-   if (scrollTop) {
-     window.scrollY > 0 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-   }
- }
- scrollTop.addEventListener('click', (e) => {
-   e.preventDefault();
-   window.scrollTo({
-     top: 0,
-     behavior: 'smooth'
-   });
- });
-
- window.addEventListener('load', toggleScrollTop);
- document.addEventListener('scroll', toggleScrollTop);
+let scrollTop = document.querySelector('.scroll-top');
+function toggleScrollTop() {
+  if (scrollTop) {
+    window.scrollY > 0 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  }
+}
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
